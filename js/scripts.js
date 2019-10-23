f071d40764299cf44f7f2a1760e18a0e731d8fd1
Willemijn Bresjer <wbbresjer@gmail.com>
2019-10-23 19:05:26 +0000

    Add starter files

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// 1: Set the text of the <h1> element
+
+
+// 2: Set the color of the <h1> to a different color
+
+
+// 3: Set the content of the '.desc' paragraph
+// The content should include at least one HTML tag
+
+
+// 4: Set the class of the <ul> to 'list'
+
+
+// 5: Create a new list item and add it to the <ul>
+
+
+// 6: Change all <input> elements from text fields to checkboxes
+
+
+// 7: Create a <button> element, and set its text to 'Delete'
+// Add the <button> inside the '.extra' <div>
+
+
+// 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
